005ebf958af51e32fc5d1fa6dcbb41f94503de22
isdmitriev <isdmitriev2@gmail.com>
2025-01-29 12:15:28 +0200

    29.0.2025(add_ci_yml)

--- tests/test_intercom_api_service.py
+++ tests/test_intercom_api_service.py
@@ -32,29 +32,29 @@ def test_add_admin_note_to_conversation():
     assert result[0] == 200
 
 
-def test_process():
-    user_id: str = "6798a0c79645a8b3711b89d3"
-    admin_id: str = "8028082"
-    create_conversation_response: str = CLIENT.create_conversation(
-        user_id=user_id, message="good day!"
-    )
-    status = create_conversation_response[0]
-    json_data = create_conversation_response[1]
-    assert status == 200
-    new_conversatin_id: str = json_data.get("conversation_id", "")
-    attach_admin_to_conversation_response = CLIENT.attach_admin_to_conversation(
-        conversation_id=new_conversatin_id, admin_id=admin_id
-    )
-    assert attach_admin_to_conversation_response[0] == 200
-    test_add_admin_message_to_conversation_response = (
-        CLIENT.add_admin_message_to_conversation(
-            conversation_id=new_conversatin_id,
-            admin_id=admin_id,
-            message="admin message",
-        )
-    )
-    assert test_add_admin_message_to_conversation_response[0] == 200
-    add_admin_note_to_conversation_response = CLIENT.add_admin_note_to_conversation(
-        conversation_id=new_conversatin_id, note="note", admin_id=admin_id
-    )
-    assert add_admin_note_to_conversation_response[0] == 200
+# def test_process():
+#     user_id: str = "6798a0c79645a8b3711b89d3"
+#     admin_id: str = "8028082"
+#     create_conversation_response: str = CLIENT.create_conversation(
+#         user_id=user_id, message="good day!"
+#     )
+#     status = create_conversation_response[0]
+#     json_data = create_conversation_response[1]
+#     assert status == 200
+#     new_conversatin_id: str = json_data.get("conversation_id", "")
+#     attach_admin_to_conversation_response = CLIENT.attach_admin_to_conversation(
+#         conversation_id=new_conversatin_id, admin_id=admin_id
+#     )
+#     assert attach_admin_to_conversation_response[0] == 200
+#     test_add_admin_message_to_conversation_response = (
+#         CLIENT.add_admin_message_to_conversation(
+#             conversation_id=new_conversatin_id,
+#             admin_id=admin_id,
+#             message="admin message",
+#         )
+#     )
+#     assert test_add_admin_message_to_conversation_response[0] == 200
+#     add_admin_note_to_conversation_response = CLIENT.add_admin_note_to_conversation(
+#         conversation_id=new_conversatin_id, note="note", admin_id=admin_id
+#     )
+#     assert add_admin_note_to_conversation_response[0] == 200
