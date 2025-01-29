fab77180d5c499921b995fac5cf6c5e26e34bbb9
isdmitriev <isdmitriev2@gmail.com>
2025-01-29 11:07:01 +0200

    29.0.2025(change_tests)

--- tests/test_intercom_api_service.py
+++ tests/test_intercom_api_service.py
@@ -9,3 +9,24 @@ def test_get_admins():
     status_code, data = result
 
     assert status_code == 200
+
+
+def test_add_admin_message_to_conversation():
+    admin_id: str = "8028082"
+    conversation_id: str = "6"
+    message: str = "i am Isdmitriev2@gmail.com"
+
+    result = CLIENT.add_admin_message_to_conversation(
+        admin_id=admin_id, conversation_id=conversation_id, message=message
+    )
+    assert result[0] == 200
+
+
+def test_add_admin_note_to_conversation():
+    admin_id: str = "8028082"
+    conversation_id: str = "6"
+    note: str = "note from Isdmitriev2@gmail.com"
+    result = CLIENT.add_admin_note_to_conversation(
+        conversation_id=conversation_id, note=note, admin_id=admin_id
+    )
+    assert result[0] == 200
